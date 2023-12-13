9f452b6c2bd757e451c6f26d60242cc4707b4074
MarcelFeo <marcelfeo29@gmail.com>
2023-12-13 12:42:26 -0300

    tratamento de erro

---
+++ src/main/java/com/banco/infra/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.banco.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneral(Exception exception) {
+        return ResponseEntity.internalServerError().build();
+    }
+
+
+}
